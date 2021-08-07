aeec7d7786b3e98c8e9a98098a22b82cbdfd8d42
Lautaro <lautarosoliani@hotmail.com>
2021-08-06 23:57:49 -0300

    14 onProgress

---
+++ myapp/src/components/ControlPresupuestario.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react"
+
+const ControlPresupuestario = ({ presupuesto, restante }) => {
+    return (
+        <Fragment>
+            <div className="alert alert-primary">Budget: ${presupuesto} </div>
+            <div className="alert">Remaining: ${restante}</div>
+        </Fragment>
+    )
+}
+
+export default ControlPresupuestario
